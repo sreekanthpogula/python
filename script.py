6c349e727f74a744ef856729f6c588063ddad957
sreekanth.pogula <sreekanth.pogula@senecaglobal.com>
2022-12-13 16:03:14 +0530

    added the datatypes, iterators, generators practice code files

---
+++ script.py
@@ -0,0 +1,69 @@
+# hello world program
+print("hello world!")
+# indentation
+x = 1
+if x == 1:
+    print("x is 1.")
+# variables and types
+# numbers
+pyint = 7
+print(pyint)
+# float
+pyfloat = 7.0
+print(pyfloat)
+
+pyfloat = float(7)
+print(pyfloat)
+# strings
+mystring = "dont know how to create a string"
+print(mystring)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+a, b = 3, 4
+print(a, b)
+
+one = 1
+two = 2
+hello = "hello"
+print(one + two)
+
+# list
+mylist = [1, 2, 3]
+mylist.append(hello)
+mylist.append(world)
+print(mylist)
+for x in mylist:
+    print(x)
+pylist = [1, 2, 3]
+# Arithmetic Operators
+a = 2 + 3
+b = 2 - 3
+c = 2 * 3
+d = 2 / 3
+e = 2 % 3
+f = 2 ** 3
+g = 2 & 3
+h = 2 ^ 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a & b)
+print(a ^ b)
+print(a | b)
+
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+
+
