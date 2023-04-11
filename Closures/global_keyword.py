7d7206e88e228db06824af361462a95b4762214a
seneca <sreekanth.pogula@senecaglobal.com>
2023-04-11 15:57:55 +0530

    Added the modified code files

---
+++ Closures/global_keyword.py
@@ -0,0 +1,19 @@
+# define global variable 
+global_var = 10
+
+def my_function():
+    # define local variable
+    local_var = 20
+
+    # modify global variable value 
+    global global_var
+    global_var = 30
+
+# print global variable value
+print(global_var)
+
+# call the function and modify the global variable
+my_function()
+
+# print the modified value of the global variable
+print(global_var)
