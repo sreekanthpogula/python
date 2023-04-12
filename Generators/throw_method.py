7b3e0ff44c3a47c98afb6acd99a58e79640ba7ef
seneca <sreekanth.pogula@senecaglobal.com>
2023-04-12 11:52:58 +0530

    Added the modified code files

--- Generators/throw_method.py
+++
@@ -1,9 +0,0 @@
-from Generators.generator_methods import getPrimes
-
-
-prime_gen = getPrimes()
-
-for x in prime_gen:
-    if x > 10:
-        prime_gen.throw(ValueError, "I think it was enough!")
-    print(x)
